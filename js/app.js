2ed6dc5a28fd48e16b271ce9b5cad0504fd6d975
Krystina O'Connor <krystinaoconnor@Krystinas-MacBook-Pro.local>
2019-12-19 14:56:06 -0600

    created and tested an example Tamagotchi with a given name, age, hunger, sleep, boredom

--- js/app.js
+++ js/app.js
@@ -3,11 +3,17 @@
 
 //TAMAGOTCHI CLASS
 class Tamagotchi {
-	constructor() {
-
+	constructor(name, age, hunger, sleepiness, boredom) {
+		this.name = name;
+		this.age = age;
+		this.hunger = hunger;
+		this.sleepiness = sleepiness;
+		this.boredom = boredom;
 	}
 }
 
+const newTamagotchi = new Tamagotchi('Krystina', 1, 3, 5, 6);
+console.log(newTamagotchi);
 
 //GAME OBJECT
 const game = {
