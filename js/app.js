fb0e7e10e51cacf522122272ca069fd015873a4d
Krystina O'Connor <krystinaoconnor@Krystinas-MacBook-Pro.local>
2019-12-19 16:42:56 -0600

    able to add character name to Tamagotchi and hide form field

--- js/app.js
+++ js/app.js
@@ -2,24 +2,46 @@
 
 
 //TAMAGOTCHI CLASS
+
+//create a Tamagotchi class to be applied to each new Tamagotchi 
 class Tamagotchi {
 	constructor(name, age, hunger, sleepiness, boredom) {
 		this.name = name;
 		this.age = age;
-		this.hunger = hunger;
-		this.sleepiness = sleepiness;
+		this.hunger = hunger; //1-10 scale
+		this.sleepiness = sleepiness;//1-10 scale
 		this.boredom = boredom;
 	}
 }
 
-const newTamagotchi = new Tamagotchi('Krystina', 1, 3, 5, 6);
-console.log(newTamagotchi);
-
 //GAME OBJECT
 const game = {
+	createTamagotchi() {
+		const buddy = new Tamagotchi();
 
+	}
 }
 
 
 //EVENT LISTENERS
+
+//listen for form (name) submissions
+$('#name-input-form').on('submit', (event) =>{
+	//need to prevent default
+	event.preventDefault();
+	const $nameInput = $('#name-input').val(); // getter/setter input
+	console.log($nameInput);
+
+	const $h1 = $('<h1></h1>');
+	$h1.text($nameInput);
+	$(document.body).append($h1);
+	$('#name-input-form').hide();
+})
+
+
+
+game.createTamagotchi();
+
+
+
 
